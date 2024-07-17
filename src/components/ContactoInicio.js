02b8eabea860671ff728b47faa3818ad5fd45a43
kolit0 <kolo03057@gmail.com>
2024-07-17 14:30:09 -0400

    contacto

--- src/components/ContactoInicio.js
+++ src/components/ContactoInicio.js
@@ -11,7 +11,7 @@ function Section2() {
 
     return (
             <div className="bg-white md:h-screen w-full flex-col justify-center lg:flex gap-20 max-lg:space-y-5 max-md:pb-5">
-               <div className="md:flex-row max-md:grid max-md:grid-cols-1 flex space-x-10 justify-center md:px-20 max-lg:space-y-5 lg:-mt-60 max-lg:pt-5">
+               <div className="md:flex-row max-md:grid max-md:grid-cols-1 flex space-x-8 justify-center md:px-20 max-lg:space-y-5 lg:-mt-60 max-lg:pt-5">
                 <div className="p-5 ml-10 bg-gray-100 h-auto w-auto flex-col  place-content-center flex gap-5">
                     <div className="border-b-4 w-full border-blue-800">
                         <h2 className="text-[min(4vw,2.5rem)] text-black">{t("contact.call")}</h2>
